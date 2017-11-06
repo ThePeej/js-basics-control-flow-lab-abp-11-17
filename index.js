dd37d519851c2e01a967151ee51a4e3a8e9e12fa
PJ Frias <peejfrias@gmail.com>
2017-11-06 01:29:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // Write your code in this file!
-function scuberGreetingForFeet(feet) {
+function scuberGreetingForFeet(distance) {
   debugger;
   if (distance <= 400){
     console.log('This one is on me!')
   } else if (distance > 2000) {
+    console.log('This one is on me!')
   }
 }
