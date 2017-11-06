4d16d53416dc4972a8d740ea2fce6c024f1d4f0a
PJ Frias <peejfrias@gmail.com>
2017-11-06 01:43:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,15 @@ function scuberGreetingForFeet(distance) {
 function ternaryCheckCity(city) {
   city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+  case 'generous':
+    return('Thank you so much.')
+    break;
+  case 'not as generous':
+    return('Thank you.')
+    break;
+  case 'thanks for everything':
+    return('Bye.')
+}
