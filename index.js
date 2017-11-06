996eaf92456a04f3af9a3ae8630c79b6562064d6
PJ Frias <peejfrias@gmail.com>
2017-11-06 01:45:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function scuberGreetingForFeet(distance) {
     return('I will gladly take your thirty bucks.')
   } else if (distance > 2500) {
     ///debugger;
-    console.log('No can do.')
+    return('No can do.')
   }
 }
 
